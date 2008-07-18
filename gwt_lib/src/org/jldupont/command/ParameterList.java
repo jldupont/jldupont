9276e1034c2604ff8fad2410f5b24a76d6723d0e
JeanLou.Dupont <JeanLou.Dupont@f8011244-a73a-0410-9179-ff93bfd83f02>
2008-07-18 19:06:51 +0000

    stdListe & ParameterList

---
+++ gwt_lib/src/org/jldupont/command/ParameterList.java
@@ -0,0 +1,48 @@
+/**
+ * ParameterList
+ * 
+ * @author Jean-Lou Dupont
+ */
+package org.jldupont.command;
+
+import org.jldupont.system.StdListe;
+
+public class ParameterList 
+	extends StdListe 
+	implements CommandParameters {
+
+	final static String thisClass = "org.jldupont.command.ParameterList";
+	
+	/*===================================================================
+	 * CONSTRUCTORS  
+	 ===================================================================*/
+	public ParameterList() {
+		super( thisClass, "default_id" );
+	}
+	
+	public ParameterList(String id ) {
+		super( thisClass, id );
+	}
+	
+	/*===================================================================
+	 * CommandParameters interface
+	 ===================================================================*/
+	
+	public Object getParameter( String key ) {
+		
+		return this.get(key);
+	}
+	
+	public void   setParameter( String key, Object o ) {
+		this.put(key, o);
+	}
+
+	
+	/*===================================================================
+	 * RECYCLING  
+	 ===================================================================*/
+	public void _clean() {
+		super._clean();
+	}
+	
+}//endclass
